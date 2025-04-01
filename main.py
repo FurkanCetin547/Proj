051b05cd3cc34c2437902cf586c218599b8c3bd1
FurkanCetin547 <furkan.cetin@sabanciuniv.edu>
2025-04-01 14:04:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from data_collection import fetch_imdb_reviews
 from sentiment_analysis import analyze_sentiment
 import pandas as pd
 
-# Step 1: Collect Data
+#  Collect Data
 reviews = fetch_imdb_reviews("tt0111161")  # Example: The Shawshank Redemption
 df = pd.DataFrame({"review_text": reviews})
 
-# Step 2: Sentiment Analysis
+#  Sentiment Analysis
 df["sentiment_score"] = df["review_text"].apply(analyze_sentiment)
 
-# Step 3: Save Data
+#  Save Data
 df.to_csv("data/movie_reviews_with_sentiment.csv", index=False)
 
 print("Analysis complete. Check the 'data' folder for results.")
