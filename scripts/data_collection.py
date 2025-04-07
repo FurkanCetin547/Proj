221120f6d71ae97d0f00de1648599a41edb2ccc4
FurkanCetin547 <furkan.cetin@sabanciuniv.edu>
2025-04-07 18:31:52 +0300

    Update data_collection.py

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -3,26 +3,24 @@
 import os
 import pandas as pd
 from imdb import IMDb
-from crawlbase import CrawlingAPI
+import requests
 from bs4 import BeautifulSoup
 
-# Initialize the IMDb and Crawlbase API
-ia = IMDb()
-crawling_api = CrawlingAPI({'token': 'YOUR_CRAWLBASE_TOKEN'})
-
-# List of movie IDs and titles
-movie_ids = ['0133093', '1375666', '0468569']
-movie_titles = ['Inception', 'The Dark Knight', 'Interstellar']
-
-# Create directories to store the raw data
+# Create necessary directories
 os.makedirs('data/raw/IMDb', exist_ok=True)
 os.makedirs('data/raw/RottenTomatoes', exist_ok=True)
+
+# Initialize IMDb object
+ia = IMDb()
+
+# List of IMDb movie IDs to scrape
+imdb_movie_ids = ['0133093', '1375666', '0468569']  # Example IDs
 
 # Function to fetch IMDb data
 def fetch_imdb_data():
     movies_metadata = []
     movies_reviews = []
-    for movie_id in movie_ids:
+    for movie_id in imdb_movie_ids:
         movie = ia.get_movie(movie_id)
         metadata = {
             'movie_id': movie_id,
@@ -52,29 +50,22 @@ def fetch_imdb_data():
 
 # Function to fetch Rotten Tomatoes data
 def fetch_rotten_tomatoes_data():
-    movies_metadata = []
-    for title in movie_titles:
-        search_url = f'https://www.rottentomatoes.com/search?search={title}'
-        response = crawling_api.get(search_url)
-        if response['headers']['pc_status'] == '200':
-            soup = BeautifulSoup(response['body'].decode('utf-8'), 'html.parser')
-            movie_link = soup.find('a', class_='unstyled articleLink')
-            if movie_link:
-                movie_url = 'https://www.rottentomatoes.com' + movie_link['href']
-                movie_page_response = crawling_api.get(movie_url)
-                if movie_page_response['headers']['pc_status'] == '200':
-                    movie_soup = BeautifulSoup(movie_page_response['body'].decode('utf-8'), 'html.parser')
-                    metadata = {
-                        'title': title,
-                        'url': movie_url,
-                        'rating': movie_soup.find('span', class_='sc-16ede01-2').text if movie_soup.find('span', class_='sc-16ede01-2') else '',
-                        'synopsis': movie_soup.find('span', class_='sc-16ede01-3').text if movie_soup.find('span', class_='sc-16ede01-3') else '',
-                        'genres': ', '.join([genre.text for genre in movie_soup.find_all('span', class_='sc-16ede01-4')]),
-                        'release_date': movie_soup.find('span', class_='sc-16ede01-5').text if movie_soup.find('span', class_='sc-16ede01-5') else ''
-                    }
-                    movies_metadata.append(metadata)
-    metadata_df = pd.DataFrame(movies_metadata)
-    metadata_df.to_csv('data/raw/RottenTomatoes/movies_metadata.csv', index=False)
+    url = 'https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    movies = soup.select('td.titleColumn')
+    movie_data = []
+    for movie in movies:
+        title = movie.select('a')[0].text
+        year = movie.select('span.secondaryInfo')[0].text.strip('()')
+        rating = movie.select('span.tMeterScore')[0].text.strip()
+        movie_data.append({
+            'title': title,
+            'year': year,
+            'rating': rating
+        })
+    df = pd.DataFrame(movie_data)
+    df.to_csv('data/raw/RottenTomatoes/movies_metadata.csv', index=False)
 
 # Fetch data
 fetch_imdb_data()
