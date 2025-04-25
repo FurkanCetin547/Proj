f096a7ab74554aec4b9363af54e96d97f804fd60
FurkanCetin547 <furkan.cetin@sabanciuniv.edu>
2025-04-25 12:52:20 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-os.system("python src/data_collection.py")
-os.system("python src/eda.py")
-os.system("python src/sentiment_analysis.py")
-os.system("python src/hypothesis_testing.py")
-os.system("python src/visualization.py")
